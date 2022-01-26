afa41865c8ee39d3487a3e90bf6b9a2d378c3a1c
mkmrgn <mike@morgan.ms>
2022-01-26 12:41:07 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 var assert = require('assert');
 
+console.log("hello")
+while(new Date().getTime() < now + 2000){ }
+console.log("world")
+
 describe('String#split', function(){
   it('should return an array', function(){
     assert(Array.isArray('a,b,c'.split(',')));
